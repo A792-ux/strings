c2829b83397e8960f7c377906608124772bade10
A792-ux <77727249+A792-ux@users.noreply.github.com>
2021-02-24 16:17:33 +0300

    Add files via upload

---
+++ strings/pythonstrings.py
@@ -0,0 +1,74 @@
+print("Bananas and mangoes are all fruits")
+q="shuttles and castles have people"
+print(q)
+print("765,455,4553")
+x="""It makes it easier to pass data between client and server by saving data in binary JSON format. The concept on which it works is collection and documentation. MongoDB provides high performance, high availability, and easy scalability because it is a cross-platform, document-oriented database. To adopt JSON-like documents which have BSON (dynamic schemes) MongoDB shuns the relational database’s table-based structure, which makes data integration for certain types of applications faster and easier."""
+print(x)
+print(len(x))
+print('john asinami')
+a="bananas"
+b="orange"
+print(a>b)
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a==b)
+print(a+b)
+print(a*3)
+print('this is ann\'s spam')
+print("this is anne's spam")
+a="5"
+b="10"
+c=a+b
+print(c)
+print( "John" + "was" + ( "here" *3 ) )
+
+
+
+#indexing a string
+z="adamulalo"
+print(z[3:5])
+print(z[1])
+print(z[-1])
+print(z[0])
+print(z[1:8])
+print(z[-1:-3])
+y="temple mount"
+print(y[1:5],y[7:11],y[4:8])
+print(z[:])
+print(y[:])
+print(y[:5],y[1:],y[::2])
+s="mrcatawambous"
+print(len(s))
+
+
+
+a=20.33
+b=75.76
+print("the sum of %f and %f is %f"%(a,b,a+b))
+x="Johnny"
+y="Walker"
+print("the sum of %s and %s is %s"%(x,y,x+y))
+#string methods
+x="hello world"
+print(len(x))
+
+txt="this is my very own world"
+x=txt.center(49)
+print(x)
+txt="this is my very own world"
+x=txt.capitalize()
+print(x)
+txt = "Audi Q8"
+x = txt.lstrip()
+print("Of all cars", x, "is my favorite")
+txt= "HOLLISTER MARR carrow"
+x=txt.lower()
+print(x)
+txt="Good morning Mr. Tuvanui"
+kuba=txt.maketrans("T","S")
+print(txt.translate(kuba))
+
+chis="We love mangoes"
+r=chis.replace("mangoes","pigs")
+print(r)
